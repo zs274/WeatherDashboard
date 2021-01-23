c8688e46a985cb492502b620a104583e41be9110
Zarin Salim <zarin_96@hotmail.co.uk>
2021-01-23 03:09:06 +0000

    started function to get and display current weather

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+$(document).ready(function() {
+
 var searchEl = document.getElementById("search-btn");
 var clearEl = document.getElementById("clear-btn");
-var enterCity = document.getElementById("enter-city");
+var inputEl = document.getElementById("input-city");
 var currentWeather = document.getElementById("current-weather");
-var cityNameEl = document.getElementById("clear-btn");
+var cityNameEl = document.getElementById("city-name");
 var currentPicEl = document.getElementById("current-pic");
 var tempEl = document.getElementById("temperature");
 var humidityEl = document.getElementById("humidity");
@@ -10,3 +12,31 @@ var windEl = document.getElementById("wind-speed");
 var uvEl = document.getElementById("uv-index");
 var currentPicEl = document.getElementById("current-pic");
 var fiveDay = document.getElementById("five-header");
+
+var APIKey = "02b7a9ea356212ecbd2304bcb80a5365"
+
+function getWeather(cityName) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response){
+        var currentDate = new Date (response.data.dt*1000);
+        var day = currentDate.getDate();
+        var month = currentDate.getMonth();
+        var year = currentDate.getFullYear();
+        cityNameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+        var weatherIcon = response.data.weather[0].icon;
+        $("#weatherIcon").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+        $("#tempEl").html() = "Temperature:" + response.data.main.temp;
+        $("#humidityEl").html() = "Humidity:" + response.data.main.humidity + "%";
+        $("windEl").html() = "Wind Speed:" + response.data.wind.speed + "MPH";
+        
+        var lat = response.data.coord.lat;
+        var lon = response.data.coord.lon;
+
+        
+    })
+}
+
+}
